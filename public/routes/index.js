0ca7936708034855c4e712da957f08d14d586cdc
Andrea <andreamayhirji@gmail.com>
2018-10-01 04:27:32 +0000

    Changed folder structure -- moved partials folder into views folder.

--- public/routes/index.js
+++ public/routes/index.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
+        //res.render is used to display the appropriate pages.
         res.render('pages/index');
     });
 
